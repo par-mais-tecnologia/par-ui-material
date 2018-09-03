c3b798a2ed371183820e3e65f58dbdd497037d9f
Gabriel Stedile <gabriel.stedile@parmais.com.br>
2018-09-03 08:24:13 -0300

    Infrastructure Config

--- src/components/Inputs/DatePicker/datePicker.test.jsx
+++ src/components/Inputs/DatePicker/datePicker.test.jsx
@@ -1,14 +1,23 @@
 import React from "react";
-import { configure, shallow } from 'enzyme'
+import { configure, shallow, render } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import DatePicker from './datePicker'
 import MomentUtils from "material-ui-pickers/utils/moment-utils";
-import expect from 'expect'
-
 configure({ adapter: new Adapter() });
+
 export const test = describe('DatePicker', () => {
         it('shallow DatePicker', () => {
             const wrapper = shallow(<DatePicker dateUtilityLibrary = {MomentUtils}/>);
             expect(wrapper).toBeTruthy();
-        })
-    })
+        });
+
+        it('shallow DatePicker 2', () => {
+            const wrapper = shallow(<DatePicker dateUtilityLibrary = {MomentUtils}/>);
+            expect(wrapper).toMatchSnapshot();
+        });
+
+        it('shallow DatePicker 3', () => {
+            const wrapper = render.create(<DatePicker dateUtilityLibrary = {MomentUtils}/>).toJSON();
+            expect(wrapper).toMatchSnapshot();
+        });
+    });
