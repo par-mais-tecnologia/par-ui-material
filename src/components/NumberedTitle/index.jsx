fe4c5f711c796830ab13615a097be1a1118f0807
Rui Cavaleiro <r2cavaleiro@gmail.com>
2018-09-10 18:10:44 -0300

    Numbered title

---
+++ src/components/NumberedTitle/index.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core';
+
+const styles = {
+  headline: {
+    fontSize: '48px',
+    color: '#636363',
+    fontWeight: '500',
+    padding: '0px 4px 0px 4px'
+  },
+  title: {
+    fontSize: '20px',
+    color: '#636363',
+    fontWeight: '300'
+  },
+  subheading: {
+    fontSize: '16px',
+    color: '#D4426A',
+    fontWeight: '800'
+  }
+}
+
+function NumberedTitle(props) {
+  const {classes} = props;
+  return (
+    <Grid container direction="column">
+      <Grid  container direction="row" alignItems="center">
+        <Typography variant="headline" classes={{headline: classes.headline}}>
+          {props.number}
+        </Typography>
+        <Grid direction="column">
+          <Typography variant="title" classes={{title: classes.title}}>
+            {props.title}
+          </Typography>
+          <Typography variant="subheading" classes={{subheading: classes.subheading}}>
+            {props.subtitle}
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid item>
+        {props.children}
+      </Grid>
+    </Grid>
+  )
+}
+
+export default withStyles(styles)(NumberedTitle);
---
+++ src/components/stories/NumberedTitle.stories.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import NumberedTitle from '../NumberedTitle';
+
+const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Curabitur quis mattis turpis, a eleifend enim. Vestibulum ante ipsum primis 
+in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent augue est, 
+pharetra sit amet dolor pulvinar, porta interdum arcu. Suspendisse mollis sem 
+quis ante ultrices, ac imperdiet ipsum consectetur. Cras ultrices venenatis erat 
+vel sollicitudin. Suspendisse vulputate convallis viverra. Nam quis neque ut 
+tellus vulputate tincidunt. Nam justo elit, placerat vel lacus sed, pellentesque 
+suscipit libero. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+per inceptos himenaeos.Integer leo orci, mattis et diam fringilla, accumsan accumsan 
+ipsum. Integer pretium sem sit amet neque lobortis vehicula. Aenean ut iaculis velit. 
+Quisque interdum lobortis porta. Maecenas sit amet neque eros. Vestibulum ut dictum odio, 
+quis elementum tortor. Mauris eleifend tortor vel diam varius, at congue erat cursus.
+Pellentesque ut elit laoreet, aliquet augue at, tempor nisi.`
+
+storiesOf('NumberedTitle', module)
+  .add('shallow', () => {
+    return (
+      <div style={{width:'400px', padding: '10px'}}>
+        <NumberedTitle
+          number={7}
+          title={`Suas`}
+          subtitle={`finanças`}
+        >
+          {lorem}
+        </NumberedTitle>
+      </div>
+    )
+  })
