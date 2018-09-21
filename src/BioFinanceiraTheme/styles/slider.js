355fe67c2938acf44f76187438f7b6863d5cb2df
Rui Cavaleiro <r2cavaleiro@gmail.com>
2018-09-21 10:47:36 -0300

    PCP-1152: fix

--- src/BioFinanceiraTheme/styles/slider.js
+++ src/BioFinanceiraTheme/styles/slider.js
@@ -3,13 +3,13 @@ export default {
     height: '9px',
     background: 'linear-gradient(to right, #8ACAC7 0%, #347A7C 100%)',
     borderRadius: '10px',
-    boxShadow: '0px 0px 1px #000000'
+    boxShadow: '1px 1px 1px #9C9C9C'
   },
   trackAfter: {
     background: 'white'
   },
   thumb: {
     backgroundColor: 'white',
-    boxShadow: '0px 0px 1px #000000'
+    boxShadow: '0px 0px 1px #9C9C9C'
   }
 }
--- src/Slider/Slider.jsx
+++ src/Slider/Slider.jsx
@@ -8,7 +8,7 @@ export default class SliderPar extends Component {
   getTypo = (max) => {
     let typos = []
     for (let i = 1; i <= max; i++) {
-      typos.push(<Typography variant='body2'>{i}</Typography>)
+      typos.push(<Typography variant='body1'>{i}</Typography>)
     }
     return typos
   }
