36bd61b74a4627b712e35f1430cd7d18f8b5a4f0
danilosetubal <danilosetubal@gmail.com>
2018-09-24 17:13:49 -0300

    PCP-1136 - Adjustments in BioInitialScreen

--- src/BioInitialScreen/BioInitialScreen.jsx
+++ src/BioInitialScreen/BioInitialScreen.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 
-import '../Core/styles/index.css'
 import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid/Grid'
+
+import '../Core/styles/index.css'
 
 const BioInitialScreen = (props) => {
   return (
-    <div className='flex justify-center items-center flex-wrap w-100 h-100'>
-      <div className='logo pv0 pv4-ns ph4 ph0-ns tc'>
-        <img
-          className='w-60 w-80-ns'
-          src='https://static.parmais.com.br/images/see-logo.svg.png'
-        />
-      </div>
-      <div className='description pa4-ns mw6-ns w-100'>
+    <Grid container justify='center' alignItems='center' wrap className='w-100 h-100'>
+      <Grid item className='logo pv0 pv4-ns ph4 ph0-ns tc'>
+        <img className='w-60 w-80-ns' src='https://static.parmais.com.br/images/see-logo.svg.png' />
+      </Grid>
+      <Grid item className='description pa4-ns mw6-ns w-100'>
         <h1 className='white brandon-light ttu mt2 fw1'><span className='brandon-bold'>Bio</span> Financeira</h1>
-        <div className='white roboto-light lh-copy'>
+        <Grid className='white roboto-light lh-copy'>
           <p>
             A BIO Financeira é a soama de seu perfil psicológico frente aos
             investimentos, momento de vida e capacidade financeira e patrimonial.
@@ -27,14 +26,14 @@ const BioInitialScreen = (props) => {
             Ao clicar em "Começar", você concorda com a nossa
             <i><b> política de privacidade</b></i>.
           </p>
-        </div>
-        <div className='flex justify-end'>
+        </Grid>
+        <Grid container justify='flex-end' className='mt4 mt0-ns'>
           <Button variant='contained' color='secondary'>
-            COMEÇAR
+            COMECE AGORA
           </Button>
-        </div>
-      </div>
-    </div>
+        </Grid>
+      </Grid>
+    </Grid>
   )
 }
 
