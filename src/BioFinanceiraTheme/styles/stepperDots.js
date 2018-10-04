bd100cf7844c736bf657636e5a29f7fd715d4b9d
Rui Cavaleiro <r2cavaleiro@gmail.com>
2018-10-04 18:29:28 -0300

    PCP-1169

--- src/BioFinanceiraTheme/styles/stepperDots.js
+++ src/BioFinanceiraTheme/styles/stepperDots.js
@@ -1,12 +1,15 @@
 export default {
   root: {
-    background: '#5EB8C0'
+    background: '#5EB8C0',
+    justifyContent: 'space-evenly'
   },
   dot: {
-    background: 'white'
+    background: 'white',
+    margin: '0px 6px 0px 6px',
+    transform: 'scale(1.2)'
   },
   dotActive: {
-    background: 'radial-gradient(black 0, black 25%, white 50%, white 100%)'
+    background: 'radial-gradient(#1D585D 0, #1D585D 35%, white 50%, white 100%)'
   },
   label: {
     color: 'white'
